a186eb7318427c44083bc697a30fd5de9439bf26
Jefferson <jeffersonf.l@outlook.com>
2022-11-23 23:12:24 -0300

    add contents in ex011.py and ex012.py

--- Exercicios/ex011.py
+++ Exercicios/ex011.py
@@ -1,2 +1,6 @@
 # Lê a altura e a largura de uma parede em metros, calcula a área e a quantidade de tinta para pintá-la, cada litro pinta 2 metros quadrados
 
+a = float(input('Digite a altura da parede: '))
+l = float(input('Digite a largura da parede: '))
+
+print('Sua parede tem uma dimensão de {:.2f}x{:.2f} e sua área é de {:.2f}m².\nPara pintá-la, você precisará de {:.2f}l de tinta'.format(a, l, a*l,(a*l)/2))
--- Exercicios/ex012.py
+++ Exercicios/ex012.py
@@ -1,1 +1,4 @@
 # Lê um valor e mostra com 5% de desconto
+
+p = float(input('Qual o preço do produto?\n R$ '))
+print('O produto que custava R$ {}, com desconto de 5% custará R$ {:.2f}.'.format(p, p-(p*5)/100))
